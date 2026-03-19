8e5a965a88c2f1a15a4df766e82c3a1a04684537
jhiggins <jackson.higgins@icloud.com>
2026-03-19 12:25:46 -0400

    Implemented RegistrationException class.

--- src/Utilities/InvalidLoginException.java
+++ src/Utilities/InvalidLoginException.java
@@ -3,6 +3,8 @@ package Utilities;
 
 /**
  * This class is used to throw errors with detaiiled messages when login errors occur
+ * 
+ * @author Jackson Higgins
  */
 public class InvalidLoginException extends Exception{
     public InvalidLoginException(String message) {
