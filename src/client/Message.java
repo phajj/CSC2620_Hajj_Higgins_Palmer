f5c50184ea12b858d1b85ac222fa8426379b08e0
Peter <petermhajj@gmail.com>
2026-03-20 12:47:35 -0400

    Fixed Message Class

--- src/client/Message.java
+++ src/client/Message.java
@@ -1,14 +1,30 @@
 package client;
 
 import java.time.LocalDateTime;
+
+/**
+ * This class represents a message in the secure chat application.
+ * It contains the content of the message, the timestamp of when it was sent, and the username of the sender.
+ * 
+ * @author Peter Hajj
+ */
+
 public class Message {
     private String content;
     private LocalDateTime timeStamp;
     private String userName;
 
+    // Constructor for creating a new message (used when sending messages)
     public Message(String content, String userName) {
         this.content = content;
         this.timeStamp = LocalDateTime.now();
+        this.userName = userName;
+    }
+    
+    // Constructor for creating a message with a specific timestamp (used when receiving messages)
+    public Message(String content, LocalDateTime timeStamp, String userName) {
+        this.content = content;
+        this.timeStamp = timeStamp;
         this.userName = userName;
     }
 
@@ -31,14 +47,5 @@ public class Message {
     }
     public void setUserName(String userName) {
         this.userName = userName;
-    }
-    
-    //TODO: sendMesesage and receiveMessage
-    public void sendMessage(){
-        
-    }
-
-    public void receiveMessage(){
-        
     }
 }
