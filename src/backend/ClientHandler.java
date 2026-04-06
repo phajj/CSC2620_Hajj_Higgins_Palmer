02192426dacec62ed53fddd556f7bbd61f8bf64a
jhiggins <jhigginsswe@gmail.com>
2026-04-06 16:43:13 -0400

    Created ConnectionChecker class to check if the server is still reachable from the client. If the server is unreachable the GUI updates its status to disconnected.

--- src/backend/ClientHandler.java
+++ src/backend/ClientHandler.java
@@ -12,43 +12,47 @@ import java.net.Socket;
  * @author Jackson Higgins
  */
 public class ClientHandler extends Thread {
-    private Socket socket;
-    private BufferedReader reader;
-    private PrintWriter sender;
+  private Socket socket;
+  private BufferedReader reader;
+  private PrintWriter sender;
 
-    public ClientHandler(Socket client) throws IOException {
-        this.socket = client;
-        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()) );
-        this.sender = new PrintWriter(socket.getOutputStream(), true);
-    }
+  public ClientHandler(Socket client) throws IOException {
+    this.socket = client;
+    this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    this.sender = new PrintWriter(socket.getOutputStream(), true);
+  }
 
-    @Override
-    public void run() {
-        try {
-            String messageString;
-            while ((messageString = reader.readLine()) != null) {
-                System.out.println("Received message from " + socket.getRemoteSocketAddress());
-                Server.broadcast(messageString);
-                
-            }
-        } catch (IOException e) {
-            System.out.println("Connection lost.");
-        } finally {
-            Server.disconnectClient(this); // Remove this client from list of clients to be broatcast to
-            try {
-                socket.close(); // Close socket
-            } catch (IOException e) {
-                e.printStackTrace();
-            } 
+  @Override
+  public void run() {
+    try {
+      String messageString;
+      while ((messageString = reader.readLine()) != null) {
+        if (messageString.equals(":ping")) {
+          sender.println("true"); // Alerts server is still online
+        } else {
+          System.out.println("Received message from " + socket.getRemoteSocketAddress());
+          Server.broadcast(messageString);
         }
-    }
 
-    /**
-     * This method will send a message over the connection back to the client
-     * 
-     * @param message Message to be transmitted
-     */
-    public void sendMessage(String messageString) {
-        sender.println(messageString); // Send message to client
+      }
+    } catch (IOException e) {
+      System.out.println("Connection lost.");
+    } finally {
+      Server.disconnectClient(this); // Remove this client from list of clients to be broatcast to
+      try {
+        socket.close(); // Close socket
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
+
+  /**
+   * This method will send a message over the connection back to the client
+   * 
+   * @param message Message to be transmitted
+   */
+  public void sendMessage(String messageString) {
+    sender.println(messageString); // Send message to client
+  }
 }
--- src/client/Receiver.java
+++ src/client/Receiver.java
@@ -8,41 +8,46 @@ import java.net.Socket;
 import Utilities.MessageHelper;
 
 /**
- * This class is a thread that is specifically for receiving messages from the connection and updating the GUI with the messages
+ * This class is a thread that is specifically for receiving messages from the
+ * connection and updating the GUI with the messages
  * 
  * @author Jackson Higgins
  */
-public class Receiver extends Thread{
-    BufferedReader receiver;
-    Socket socket;
-    String encryptedMessage;
-    MessageHelper messageHelper;
-    Encrypter encrypter;
-    GUI gui;
+public class Receiver extends Thread {
+  BufferedReader receiver;
+  Socket socket;
+  String encryptedMessage;
+  MessageHelper messageHelper;
+  Encrypter encrypter;
+  GUI gui;
 
-    public Receiver(Socket socket, Encrypter encrypter, GUI gui) throws IOException {
-        this.socket = socket;
-        this.receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        this.messageHelper = new MessageHelper();
-        this.encrypter = encrypter; 
-        this.gui = gui;
-    }
+  public Receiver(Socket socket, Encrypter encrypter, GUI gui) throws IOException {
+    this.socket = socket;
+    this.receiver = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    this.messageHelper = new MessageHelper();
+    this.encrypter = encrypter;
+    this.gui = gui;
+  }
 
-    @Override
-    public void run() {
-        try {
-            while ((encryptedMessage = receiver.readLine()) != null) {
-                String messageString = encrypter.decryptMessage(encryptedMessage); // Decrypt message to string
-                Message message = messageHelper.toMessage(messageString); // Convert message string to a Message object
-                System.out.println("Received message from : " + socket.getInetAddress());
-                System.out.println("Message: " + messageString);
-                //Update gui and log file. e.g. gui.putMessage(), log.logMessage()
-                
-                gui.showChatMessage(message); // Show the message in the chat area of the GUI
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+  @Override
+  public void run() {
+    try {
+      while ((encryptedMessage = receiver.readLine()) != null) {
+        if (encryptedMessage.equals("true")) { // If we received a response to our ping
+          ConnectionChecker.setConnectionStatus(true);
+          continue;
         }
+        String messageString = encrypter.decryptMessage(encryptedMessage); // Decrypt message to string
+        Message message = messageHelper.toMessage(messageString); // Convert message string to a Message object
+        System.out.println("Received message from : " + socket.getInetAddress());
+        System.out.println("Message: " + messageString);
+        // Update gui and log file. e.g. gui.putMessage(), log.logMessage()
+
+        gui.showChatMessage(message); // Show the message in the chat area of the GUI
+
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
