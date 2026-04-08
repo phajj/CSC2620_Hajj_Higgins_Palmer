bfc4957710babe72e9b5096aa7375858b595bfa4
jhiggins <jhigginsswe@gmail.com>
2026-04-08 11:23:49 -0400

    Updated ClientHandler and Server classes to broadcast messages to only the specific group that the message was received from.

--- src/backend/ClientHandler.java
+++ src/backend/ClientHandler.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+
+import client.Encrypter;
+import Utilities.MessageHelper;
 
 /**
  * This class is a thread that handles exactly one socket and its operations
@@ -15,11 +19,25 @@ public class ClientHandler extends Thread {
   private Socket socket;
   private BufferedReader reader;
   private PrintWriter sender;
+  private Encrypter encrypter;
+  private MessageHelper messageHelper;
+  private ArrayList<String> groups;
 
   public ClientHandler(Socket client) throws IOException {
     this.socket = client;
     this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     this.sender = new PrintWriter(socket.getOutputStream(), true);
+    this.encrypter = new Encrypter();
+    this.messageHelper = new MessageHelper();
+    this.groups = new ArrayList<>();
+  }
+
+  public ArrayList<String> getGroups() {
+    return this.groups;
+  }
+
+  public void addGroup(String group) {
+    this.groups.add(group);
   }
 
   @Override
@@ -30,8 +48,9 @@ public class ClientHandler extends Thread {
         if (messageString.equals(":ping")) {
           sender.println("true"); // Alerts server is still online
         } else {
+          String group = messageHelper.toMessage(encrypter.decryptMessage(messageString)).getGroup();
           System.out.println("Received message from " + socket.getRemoteSocketAddress());
-          Server.broadcast(messageString);
+          Server.broadcast(messageString, group);
         }
 
       }
