135d00ee019f0f3103bd71ad5ed5f3e99e05323a
jhiggins <jhigginsswe@gmail.com>
2026-04-08 11:28:43 -0400

    Updated Message class to store the group the the message is intended for.

--- src/client/Message.java
+++ src/client/Message.java
@@ -4,48 +4,64 @@ import java.time.LocalDateTime;
 
 /**
  * This class represents a message in the secure chat application.
- * It contains the content of the message, the timestamp of when it was sent, and the username of the sender.
+ * It contains the content of the message, the timestamp of when it was sent,
+ * and the username of the sender.
  * 
  * @author Peter Hajj
  */
 
 public class Message {
-    private String content;
-    private LocalDateTime timeStamp;
-    private String userName;
+  private String content;
+  private LocalDateTime timeStamp;
+  private String userName;
+  private String group;
 
-    // Constructor for creating a new message (used when sending messages)
-    public Message(String content, String userName) {
-        this.content = content;
-        this.timeStamp = LocalDateTime.now();
-        this.userName = userName;
-    }
-    
-    // Constructor for creating a message with a specific timestamp (used when receiving messages)
-    public Message(String content, LocalDateTime timeStamp, String userName) {
-        this.content = content;
-        this.timeStamp = timeStamp;
-        this.userName = userName;
-    }
+  // Constructor for creating a new message (used when sending messages)
+  public Message(String content, String userName, String group) {
+    this.content = content;
+    this.timeStamp = LocalDateTime.now();
+    this.userName = userName;
+    this.group = group;
+  }
 
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
+  // Constructor for creating a message with a specific timestamp (used when
+  // receiving messages)
+  public Message(String content, LocalDateTime timeStamp, String userName, String group) {
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.userName = userName;
+    this.group = group;
+  }
 
-    public LocalDateTime getTimeStamp() {
-        return timeStamp;
-    }
-    public void setTimeStamp(LocalDateTime timeStamp) {
-        this.timeStamp = timeStamp;
-    }
+  public String getContent() {
+    return content;
+  }
 
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public LocalDateTime getTimeStamp() {
+    return timeStamp;
+  }
+
+  public void setTimeStamp(LocalDateTime timeStamp) {
+    this.timeStamp = timeStamp;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getGroup() {
+    return this.group;
+  }
+
+  public void setGroup(String group) {
+    this.group = group;
+  }
 }
